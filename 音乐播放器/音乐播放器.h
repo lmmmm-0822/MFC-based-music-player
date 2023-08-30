1078b1c1fa09f0c5c277b0ffa25a3ca79f43f6e7
lmmmm-0822 <1575046876@qq.com>
2023-08-30 18:33:36 +0800

    first

---
+++ 音乐播放器/音乐播放器.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+
+class C音乐播放器App : public CWinApp
+{
+public:
+	C音乐播放器App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C音乐播放器App theApp;
